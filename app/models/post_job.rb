79643ab5454fea72ead0dd124858e65b665eaa8f
gentle0219 <gentle0219@hotmail.com>
2013-03-04 11:19:48 +0800

    complete posting pages

--- app/models/post_job.rb
+++ app/models/post_job.rb
@@ -1,24 +1,28 @@
 class PostJob < ActiveRecord::Base
-	attr_accessible :user_id, :job_description, :job_title, :job_type, :skills_attributes
+	attr_accessible :user_id, :job_description, :job_title, :job_type, :skills_attributes, :skill_tokens
   belongs_to :user
   has_many :skills, :as => :skillable, :dependent => :destroy
+  has_many :skill_lists, :through => :skills
 
   accepts_nested_attributes_for :skills
   validates :job_description, presence: true
   validates :job_title, :presence => true, :length => {:maximum => 50, :message => "Please limit length of words to less than 50 characters each."}
 
+  attr_reader :skill_tokens
+  def skill_tokens=(ids)
+    self.skill_list_ids = ids.split(",")
+  end
+
   JOB_TYPE = ['Full Time','Contract','Contract to Hire']
   def get_jobtype
   	JOB_TYPE[self.job_type.to_i]
   end
-  def skill_list_ids=(ids)
-    self.skill_list_ids = ids.split(",")
-  end
+
   def get_skill    
-    skill_types = ""
-    SkillList.find(self.skills.map(&:skill_list_id)).each do |skill|
-      skill_types = skill.name + "\n"
+    skills = ""
+    self.skill_lists.each do |skill_name|
+      skills += skill_name.name + " "
     end
-    return skill_types
+    skills
   end
 end
--- app/models/post_mentor.rb
+++ app/models/post_mentor.rb
@@ -1,17 +1,23 @@
 class PostMentor < ActiveRecord::Base
   belongs_to :user
   has_many :skills, :as => :skillable
-  attr_accessible :user_id, :job_description, :job_interview, :job_title, :skills_attributes
+  has_many :skill_lists, :through => :skills
+  attr_accessible :user_id, :job_description, :job_interview, :job_title, :skills_attributes, :skill_tokens
 
   accepts_nested_attributes_for :skills
   validates :job_description, presence: true
   validates :job_title, :presence => true, :length => {:maximum => 50}
+  
+  attr_reader :skill_tokens
+  def skill_tokens=(ids)
+    self.skill_list_ids = ids.split(",")
+  end
 
   def get_skill    
-    skill_types = ""
-    SkillList.find(self.skills.map(&:skill_list_id)).each do |skill|
-      skill_types = skill.name + "\n"
+    skills = ""
+    self.skill_lists.each do |skill_name|
+      skills += skill_name.name + " "
     end
-    return skill_types
+    skills
   end
 end
