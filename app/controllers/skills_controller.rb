f5cbe26a15081d91e46138a99960df3097bea539
gentle0219 <gentle0219@hotmail.com>
2013-03-06 04:50:37 +0800

    modify skill input box controller

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,7 +1,7 @@
 class SkillsController < ApplicationController
 	def index
 		arg = params[:q].downcase.to_s;
-		@skills = SkillList.where("name like ?", "%#{arg}%")
+		@skills = SkillList.where("name like ?", "#{arg}%")
 		respond_to do |format|
 			format.html
 			format.json{ render :json => @skills.map(&:attributes)}
