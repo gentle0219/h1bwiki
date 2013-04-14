8c6a3008a5b0cceef2d3cbb3a254cc77b6df57de
gentle0219 <gentle0219@hotmail.com>
2013-04-15 00:36:29 +0800

    customize helper method

--- app/helpers/search_employer_helper.rb
+++ app/helpers/search_employer_helper.rb
@@ -1,2 +1,8 @@
 module SearchEmployerHelper
+	def overview_item val
+		val = val.downcase
+		return raw("<div class='res-box val-green'><p>#{val.upcase}</p></div>") if val=="good" || val == "yes"
+		return raw("<div class='res-box val-blue'><p>#{val.upcase}</p></div>") if val=="neutral"
+		return raw("<div class='res-box val-red'><p>#{val.upcase}</p></div>") if val=="no" || val == "bad"
+	end
 end
