40b94533c0b111d4eccfeda02d6b2daf430b9d05
gentle0219 <gentle0219@hotmail.com>
2013-07-24 21:14:26 +0800

    modify search home

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,7 +1,7 @@
 class SkillsController < ApplicationController
 	def index
 		arg = params[:q];
-		@skills = SkillList.where("name like ?", "%#{arg}%")
+		@skills = SkillList.where("lower(name) like lower(?)", "%#{arg}%")
 		respond_to do |format|
 			format.html
 			format.json{ render :json => @skills.map(&:attributes)}
